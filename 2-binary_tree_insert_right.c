6b14e9f3ad4dba1b5693bb4f2d5bf1a1b1416700
seanmuzungu <seanmuzungu@gmail.com>
2022-12-13 15:52:28 +0000

    CHORE: Insert Right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,25 +1,31 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_insert_right -
- *
- * Return: whatever 
+ * binary_tree_insert_right - add a node in the right of the parent
+ * if it exists it move down one level and add the new node first
+ * @parent: parent of the specified node
+ * @value: value of the node
+ * Return: NULL if it fails or the new node
  */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-binary_tree_t *node_right;
+	binary_tree_t *new_node;
 
-if (parent == NULL)
-return (NULL);
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-node_right = binary_tree_node(parent, value);
-if (node_right == NULL)
-return (NULL);
-if (parent->right)
-{
-parent->right->parent = node_right;
-node_right->right = parent->right;
-}
-parent->right = node_right;
-return (node_right);
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
 }
