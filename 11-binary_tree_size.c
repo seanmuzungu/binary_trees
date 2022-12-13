922e802abd4b310229f0e7447389d095dff444b1
seanmuzungu <seanmuzungu@gmail.com>
2022-12-13 15:59:27 +0000

    CHORE: Add size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,50 +1,22 @@
 #include "binary_trees.h"
-size_t binary_tree_size2(const binary_tree_t *tree);
-
 /**
- * binary_tree_size2 - measures the size of a binary tree
- *
- *@tree: pointer to tree
- *
- *Return: size
+ * binary_tree_size - function that return the size of a tree
+ * @tree: tree to check
+ * Return: size of tree
  */
-
-size_t binary_tree_size2(const binary_tree_t *tree)
-{
-  int cont1 = 0, cont2 = 0;
-
-  if (tree == NULL)
-    return (0);
-
-  if (tree->left != NULL)
-    {
-
-      cont1 = binary_tree_size2(tree->left);
-      cont1++;
-    }
-
-  if (tree->right != NULL)
-    {
-
-      cont2 = binary_tree_size2(tree->right);
-      cont2++;
-    }
-
-  return (cont1 + cont2);
-}
-
-/**
- * binary_tree_size - measures the size of a binary tree
- *
- *@tree: pointer to tree
- *
- *Return: size
- */
-
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
+	size_t size = 0, r = 0, l = 0;
 
-  return (binary_tree_size2(tree) + 1);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		l = binary_tree_size(tree->left);
+		r = binary_tree_size(tree->right);
+		size = r + l + 1;
+	}
+	return (size);
 }
