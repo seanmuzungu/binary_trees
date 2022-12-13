b328f083229b73ce901948637424b8fd21e967bf
seanmuzungu <seanmuzungu@gmail.com>
2022-12-13 16:01:09 +0000

    CHORE: Add leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,16 +1,22 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_leaves - counts the leaves in a tree
- *@tree: pointer to the tree
+ * binary_tree_leaves - function that returns the number of leaves in a tree
+ * @tree: tree to check
  * Return: number of leaves
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
-  if (tree->left == NULL && tree->right == NULL)
-    return (1);
-  else
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	size_t leaf = 0, l = 0, r = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		l = binary_tree_leaves(tree->left);
+		r = binary_tree_leaves(tree->right);
+		leaf = l + r;
+		return ((!l && !r) ? leaf + 1 : leaf + 0);
+	}
 }
